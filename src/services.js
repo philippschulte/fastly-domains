6ae771d611f7e78e291fb864ceca6f2229fbb4b4
philippschulte <philipp.schulte@ymail.com>
2017-07-22 21:30:53 -0700

    Refactor services.js with async/await

--- src/services.js
+++ src/services.js
@@ -1,11 +1,15 @@
+'use strict';
+
 const map = require('lodash/fp/map');
 const pick = require('lodash/fp/pick');
 const axios = require('axios');
-const url = require('./config').mainEntryPoint;
+const config = require('./config');
 
-module.exports = token => {
-  return axios.get(`${url}/service`, { headers: { 'Fastly-Key': token } })
-    .then(services => {
-      return map(pick(['version', 'id']))(services.data);
-    });
+module.exports = async token => {
+  try {
+    const services = await axios.get(`${config.mainEntryPoint}/service`, { headers: { 'Fastly-Key': token } });
+    return map(pick(['version', 'id']))(services.data);
+  } catch (e) {
+    throw new Error(`Unable to get services that use ${token}.`);
+  }
 };
