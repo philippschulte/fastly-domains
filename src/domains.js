f217f35042c602865b65e1c5f962ca7accb6b1c2
philippschulte <philipp.schulte@ymail.com>
2017-07-23 17:40:54 -0700

    Refactor domains.js with async/await

--- src/domains.js
+++ src/domains.js
@@ -1,27 +1,35 @@
+'use strict';
+
 const flow = require('lodash/fp/flow');
 const map = require('lodash/fp/map');
 const flatten = require('lodash/fp/flatten');
 const pick = require('lodash/fp/pick');
 const axios = require('axios');
-const url = require('./config').mainEntryPoint;
+const config = require('./config');
 
-module.exports = (token, services) => {
-  const fastlyServices = map(service => {
-    return axios.get(`${url}/service/${service.id}/domain`, { headers: { 'Fastly-Key': token } });
-  })(services);
+module.exports = async (token, serviceIDs) => {
+  try {
+    const servicePromises = map(service => {
+      return axios.get(
+        `${config.mainEntryPoint}/service/${service.id}/domain`,
+        { headers: { 'Fastly-Key': token } }
+      );
+    })(serviceIDs);
 
-  return Promise.all(fastlyServices)
-    .then(services => {
-      const domains = flow(
-        map(domain => domain.data),
-        flatten,
-        map(pick(['version', 'name', 'service_id']))
-      )(services);
+    const services = await Promise.all(servicePromises);
 
-      return {
-        domains,
-        number_of_domains: domains.length,
-        number_of_services: fastlyServices.length
-      };
-    });
+    const domains = flow(
+      map(domain => domain.data),
+      flatten,
+      map(pick(['version', 'name', 'service_id']))
+    )(services);
+
+    return {
+      domains,
+      number_of_domains: domains.length,
+      number_of_services: services.length
+    };
+  } catch (e) {
+    throw new Error(`Unable to get domains that use ${token}.`);
+  }
 };
