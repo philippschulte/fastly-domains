55ed32c563cc65fc5a334dadd15d11f9d66c4045
philippschulte <philipp.schulte@ymail.com>
2017-07-24 22:39:10 -0700

    Delete token property and test assertions in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 'use strict';
 
 module.exports = {
-  mainEntryPoint: 'https://api.fastly.com',
-  token: '50a2213a73c8a8811f03f9fc6410335d'
+  mainEntryPoint: 'https://api.fastly.com'
 };
--- test/config.js
+++ test/config.js
@@ -4,23 +4,11 @@ const expect = require('expect');
 const config = require('../src/config');
 
 describe('config.js', () => {
-  describe('Main Entry Point', () => {
-    it('main entry point should exist', () => {
-      expect(config.mainEntryPoint).toExist();
-    });
-
-    it('main entry point should be a string', () => {
-      expect(config.mainEntryPoint).toBeA('string');
-    });
+  it('main entry point should exist', () => {
+    expect(config.mainEntryPoint).toExist();
   });
 
-  describe('Token', () => {
-    it('token should exist', () => {
-      expect(config.token).toExist();
-    });
-
-    it('token should be a string', () => {
-      expect(config.token).toBeA('string');
-    });
+  it('main entry point should be a string', () => {
+    expect(config.mainEntryPoint).toBeA('string');
   });
 });
