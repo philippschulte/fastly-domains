a84494f3982401dd9aa4cb562a77f9ce8aa8b847
philippschulte <philipp.schulte@ymail.com>
2017-07-27 16:36:01 -0700

    Add services.response.js to test folder

---
+++ test/response/services.response.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  body: [
+    {
+      version: 3,
+      id: '5IwGxivGVTUMZ5iF0RvaHG'
+    },
+    {
+      version: 2,
+      id: '37zPl5ELMgZ2iGVv02qIac'
+    }
+  ],
+  error: {
+    401: {
+      message: 'Error: Request failed with status code 401'
+    }
+  }
+}
