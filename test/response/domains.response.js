39f2eb4b36b0b8de80d6f0345eafc35084f74d92
philippschulte <philipp.schulte@ymail.com>
2017-07-30 14:47:31 -0700

    Add domains.response.js to test folder

---
+++ test/response/domains.response.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  body: {
+    domains: [
+      {
+        version: 2,
+        name: 'fastly-domains.com',
+        service_id: '20Esr3c2mP2IO4661htKVo'
+      },
+      {
+        version: 2,
+        name: 'www.fastly-domains.com',
+        service_id: '20Esr3c2mP2IO4661htKVo'
+      }
+    ],
+    number_of_domains: 2,
+    number_of_services: 1
+  },
+  error: {
+    401: {
+      msg: 'Provided credentials are missing or invalid'
+    },
+    404: {
+      msg: 'Record not found'
+    }
+  }
+};
